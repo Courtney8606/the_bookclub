e81e761cb32ec3cc452f0ca4e5119674e1047d28
Courtney Suhr <courtneylsuhr@gmail.com>
2024-04-24 09:16:39 +0100

    database connection setup, table created with test data, folder structure created, additional dependencies installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,45 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const [array, setArray] = useState([]);
+  const [test, setTest] = useState([]);
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:5001/api/users");
     setArray(response.data.users);
   };
 
+  const fetchTest = async () => {
+    const response = await axios.get("http://localhost:5001/test");
+    setTest(response.data);
+  };
+
   useEffect(() => {
     fetchAPI();
+    fetchTest();
   }, []);
 
   return (
-    <>
-      {array.map((user, index) => (
-        <div key={index}>
-          <span>{user}</span>
-          <br></br>
-        </div>
-      ))}
-    </>
+    <BrowserRouter>
+      <Routes>{/* <Route path="/" element={<HomePage/>}/> */}</Routes>
+      <>
+        {array.map((user, index) => (
+          <div key={index}>
+            <span>{user}</span>
+            <br></br>
+          </div>
+        ))}
+        {test.map((item) => (
+          <div key={item.id}>
+            <span>{item.title}</span>
+            <br></br>
+          </div>
+        ))}
+      </>
+    </BrowserRouter>
   );
 }
 
