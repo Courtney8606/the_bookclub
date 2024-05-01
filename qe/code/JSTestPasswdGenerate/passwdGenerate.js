42c14954a2169f0c081b8b671a5fc437c308c59f
Amir Khan <ak2022dev@gmail.com>
2024-05-01 17:24:38 +0100

    Add function for generating random integer. Use to return random single character password, when one password requested. Removed previous tests that are not relevant at this stage.

--- qe/code/JSTestPasswdGenerate/passwdGenerate.js
+++ qe/code/JSTestPasswdGenerate/passwdGenerate.js
@@ -1,9 +1,19 @@
 // File: passwdGenerate.js
 
+// Following is adapted from MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// The maximum is exclusive and the minimum is inclusive
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
+}
+
 const passwdGenerate = ( maxPasswdLength, numberOfTestPasswds ) => {
+    lowercase = "abcdefghijklmnopqrstuvwxyz";
     passwds = [];
     for( currentTestPasswdNumber = 0; currentTestPasswdNumber < numberOfTestPasswds; currentTestPasswdNumber++ ) {
-        passwds.push('a');
+        randomCharNumber = getRandomInt(0, lowercase.length);
+        passwds.push(lowercase.charAt(randomCharNumber));
     }
     return passwds;
 };
--- qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
+++ qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
@@ -3,20 +3,10 @@
 const passwdGenerate = require("./passwdGenerate");
 
 describe('passwdGenerate', () => {
-    it('generates a single lowercase character for password length 1 and output number 1', () => {
-        result = passwdGenerate( 1, 1 );
-        expect(result).toEqual(['a']);
-        console.log("Actual output is ", result);
-    });
-    it('generates two lowercase characters for password length 1 and output number 2', () => {
-        result = passwdGenerate( 1, 2 );
-        expect(result).toEqual(['a', 'a']);
-        console.log("Actual output is ", result);
-    });
-    it('generates five lowercase characters for password length 1 and output number 5', () => {
-        result = passwdGenerate( 1, 5 );
-        expect(result).toEqual(['a', 'a', 'a', 'a', 'a']);
-        console.log("Actual output is ", result);
-    });
+   it('generates a random single lowercase character for password length 1 and output number 1', () => {
+       result = passwdGenerate( 1, 1 );
+       expect(result.length).toBe(1);
+       console.log("Actual output is ", result);
+   });
 });
 
