b66a652e33b4e05736c8feef011a41dfebc85c9f
Amir Khan <ak2022dev@gmail.com>
2024-05-01 17:44:10 +0100

    Pass tests for one or more single character passwords

--- qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
+++ qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
@@ -3,10 +3,14 @@
 const passwdGenerate = require("./passwdGenerate");
 
 describe('passwdGenerate', () => {
-   it('generates a random single lowercase character for password length 1 and output number 1', () => {
+   it('generates a random single, single character password', () => {
        result = passwdGenerate( 1, 1 );
        expect(result.length).toBe(1);
        console.log("Actual output is ", result);
    });
+   it('generates an arbitrary number of random, single character passwords', () => {
+       result = passwdGenerate( 1, 50 );
+        expect(result.length).toBe(50);
+        console.log("Actual output is ", result);
+    });
 });
-
