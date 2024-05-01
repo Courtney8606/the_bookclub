0e005f1f6358daa6f1151dea1e63b94955efdef7
Amir Khan <ak2022dev@gmail.com>
2024-05-01 12:13:33 +0100

    Setup npm with local install of jest, pass first blank test

---
+++ qe/code/JSTestPasswdGenerate/passwdGenerate.js
@@ -0,0 +1,2 @@
+// File: passwdGenerate.js
+
--- qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
+++ qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
@@ -1,1 +1,10 @@
 // File: passwdGenerate.test.js
+
+const passwdGenerate = require("./passwdGenerate");
+
+describe('passwdGenerate', () => {
+    it('does nothing at the moment', () => {
+
+    });
+});
+
