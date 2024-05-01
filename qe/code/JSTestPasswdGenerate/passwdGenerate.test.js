5eb62ca6fcd3b03dbce43264c7fd107a771aa8c4
Amir Khan <ak2022dev@gmail.com>
2024-05-01 19:34:23 +0100

    Use 'test' to output result of 1000 passwords of max length 1000

--- qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
+++ qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
@@ -4,6 +4,7 @@ const passwdGenerate = require("./passwdGenerate");
 
 describe('passwdGenerate', () => {
     it('has a blank test only for now', () => {
-
+        result = passwdGenerate(30, 1000);
+        console.log("Actual output is: ", result);
     });  
 });
