0ce68957df247a41952097ef04ad4345e3b3bdaa
Amir Khan <ak2022dev@gmail.com>
2024-05-01 15:35:57 +0100

    Change test matchers to use array literals with .toEqual, which was not working with .toBe

--- qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
+++ qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
@@ -5,11 +5,18 @@ const passwdGenerate = require("./passwdGenerate");
 describe('passwdGenerate', () => {
     it('generates a single lowercase character for password length 1 and output number 1', () => {
         result = passwdGenerate( 1, 1 );
-        expect(result).toEqual(expect.arrayContaining(['a']));
+        expect(result).toEqual(['a']);
+        console.log("Actual output is ", result);
     });
     it('generates two lowercase characters for password length 1 and output number 2', () => {
         result = passwdGenerate( 1, 2 );
-        expect(result).toEqual(expect.arrayContaining(['a', 'a']));
+        expect(result).toEqual(['a', 'a']);
+        console.log("Actual output is ", result);
+    });
+    it('generates five lowercase characters for password length 1 and output number 5', () => {
+        result = passwdGenerate( 1, 5 );
+        expect(result).toEqual(['a', 'a', 'a', 'a', 'a']);
+        console.log("Actual output is ", result);
     });
 });
 
