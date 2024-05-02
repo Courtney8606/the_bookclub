9d73ae979481f8d6531605b0449b5aeb7ca59a1c
Amir Khan <ak2022dev@gmail.com>
2024-05-02 11:48:16 +0100

    Add import of isPasswordValid code. Add blank test with confirming console output, ready for further testing.

--- qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
+++ qe/code/JSTestPasswdGenerate/passwdGenerate.test.js
@@ -1,5 +1,6 @@
 // File: passwdGenerate.test.js
 
+const isPasswordValid = require("./isPasswordValid");
 const passwdGenerate = require("./passwdGenerate");
 
 describe('passwdGenerate', () => {
@@ -8,3 +9,11 @@ describe('passwdGenerate', () => {
         console.log("Actual output is: ", result);
     });  
 });
+
+describe('isPasswordValid', () => {
+    it('has a blank test only for now', () => {
+        result = isPasswordValid("");
+        console.log("Actual output is: ", result);
+    });  
+});
+
